3b020605a004efdce9591452ae49ec847b076116
arimateia286 <arimateia286@gmail.com>
2026-03-15 09:19:37 -0300

    chore: revert base path in Vite configuration to root

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = '/calculadora-react/';
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: repoName
+  base: '/'
 })
